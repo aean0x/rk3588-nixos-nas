6e347401f285f211ce81e1f8512a39f176904697
aean0x <3682177+aean0x@users.noreply.github.com>
2026-01-29 16:19:38 +0100

    cleanup

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -1,5 +1,10 @@
 # Kernel configuration for ROCK5 ITX (mainline, EDK2 UEFI)
-{ lib, pkgs, settings, ... }:
+{
+  lib,
+  pkgs,
+  settings,
+  ...
+}:
 
 let
   kernelPkgs = pkgs.${settings.kernelPackage};
@@ -8,13 +13,12 @@ in
   boot.kernelPackages = kernelPkgs;
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
-  hardware.cpu.arm.enable = true;
   hardware.enableRedistributableFirmware = true;
   networking.useDHCP = lib.mkDefault true;
 
   hardware.deviceTree = {
     enable = true;
-    name   = "rockchip/rk3588-rock-5-itx.dtb";
+    name = "rockchip/rk3588-rock-5-itx.dtb";
     filter = "*-rock-5-itx*.dtb";
   };
 
@@ -27,8 +31,8 @@ in
       "rootwait"
       "earlycon"
       "consoleblank=0"
-      "console=tty1"                # primary framebuffer console
-      "console=ttyS2,115200n8"      # most common RK3588 debug UART; change baud if needed
+      "console=tty1" # primary framebuffer console
+      "console=ttyS2,115200n8" # most common RK3588 debug UART; change baud if needed
       # "dtb=/rockchip/rk3588-rock-5-itx.dtb"  # uncomment only if EDK2 not passing DTB
 
       # Optional debug / splash
@@ -38,11 +42,11 @@ in
     ];
 
     initrd.availableKernelModules = [
-      "nvme"       # NVMe
-      "mmc_block"  # SD / eMMC
-      "hid"        # USB keyboards during initrd
-      "dm_mod"     # LVM / LUKS
-      "dm_crypt"   # LUKS
+      "nvme" # NVMe
+      "mmc_block" # SD / eMMC
+      "hid" # USB keyboards during initrd
+      "dm_mod" # LVM / LUKS
+      "dm_crypt" # LUKS
       "input_leds"
       # Add rockchip_* display / DRM modules if early KMS desired (usually auto-loaded later)
     ];
